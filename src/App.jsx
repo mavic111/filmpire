0065a4ee550b75deeff087ec1e8a724e9857f915
muhammadnizamuddinaulia <muhammadnizamuddinaulia@gmail.com>
2024-05-17 22:18:45 +0000

    Add CssBaseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
 
 const App = () => (
+
   <div>
-    App
+    <CssBaseline />
+    <h1>Hello World - Filmpire</h1>
   </div>
 );
 
