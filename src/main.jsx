3ce2db5a18075f8a128a55befb66b08639b34eb5
muhammadnizamuddinaulia <muhammadnizamuddinaulia@gmail.com>
2024-06-02 21:09:36 +0000

    Redirect login callback /approved to index page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Navigate } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -11,14 +11,22 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/" element={<Movies />} />
-      <Route path="movies/:id" element={<MovieInformation />} />
-      <Route path="actors/:id" element={<Actors />} />
-      <Route path="profiles/:id" element={<Profiles />} />
+      <Route path="/movies/:id" element={<MovieInformation />} />
+      <Route path="/actors/:id" element={<Actors />} />
+      <Route path="/profiles/:id" element={<Profiles />} />
+      <Route
+        path="*"
+        element={<Navigate to="/" replace />}
+      />
     </Route>,
   ),
 );
 
-const theme = createTheme({});
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
